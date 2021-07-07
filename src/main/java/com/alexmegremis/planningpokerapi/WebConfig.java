6ac254c8127cc2911e2a34fb1388caa5f302e368
Alex Megremis <>
2021-07-07 16:46:27 +0100

    rel0.0.1

--- src/main/java/com/alexmegremis/planningpokerapi/WebConfig.java
+++ src/main/java/com/alexmegremis/planningpokerapi/WebConfig.java
@@ -1,8 +1,10 @@
 package com.alexmegremis.planningpokerapi;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.*;
 
+@Profile("devlocal")
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
