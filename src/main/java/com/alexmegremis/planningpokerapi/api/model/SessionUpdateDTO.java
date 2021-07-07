fb861d35f2706830c01cdce5c9486d8e9e291279
Alex Megremis <>
2021-07-07 04:04:57 +0100

    #6 Closing show vote results

--- src/main/java/com/alexmegremis/planningpokerapi/api/model/SessionUpdateDTO.java
+++ src/main/java/com/alexmegremis/planningpokerapi/api/model/SessionUpdateDTO.java
@@ -1,9 +1,12 @@
 package com.alexmegremis.planningpokerapi.api.model;
 
 import lombok.*;
+import org.springframework.util.StringUtils;
 
 import java.time.ZonedDateTime;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @ToString
 @Getter
@@ -17,12 +20,13 @@ public class SessionUpdateDTO implements UniqueIdentifiable {
     private final Boolean   votesVisible;
     private final Boolean   votesAlwaysVisible;
     private final Boolean   votingOpen;
+    private final String    voteResult;
     private final Long      version;
 
     @Builder
     public SessionUpdateDTO(final String id, final String name, final PlayerDTO owner,
                             final boolean ownerCanVote, final boolean votingOpen, final boolean playersVisible, final boolean votesVisible, final boolean votesAlwaysVisible,
-                            final long version, final ZonedDateTime created, final ZonedDateTime updated) {
+                            final String voteResult, final long version, final ZonedDateTime created, final ZonedDateTime updated) {
         this.id = id;
         this.name = name;
         this.owner = owner;
@@ -31,12 +35,19 @@ public class SessionUpdateDTO implements UniqueIdentifiable {
         this.playersVisible = playersVisible;
         this.votesVisible = votesVisible;
         this.votesAlwaysVisible = votesAlwaysVisible;
+        this.voteResult = voteResult;
         this.version = version;
         this.created = created;
         this.updated = updated;
     }
 
     public static SessionUpdateDTO create(final SessionDTO session) {
+        String voteResult = null;
+        if(session.getVotesVisible()) {
+            voteResult = getVoteResult(session);
+        } else {
+            voteResult = "N/A";
+        }
         SessionUpdateDTO result = SessionUpdateDTO.builder()
                                                   .id(session.getId())
                                                   .name(session.getName())
@@ -46,6 +57,7 @@ public class SessionUpdateDTO implements UniqueIdentifiable {
                                                   .votesVisible(session.getVotesVisible())
                                                   .votesAlwaysVisible(session.getVotesAlwaysVisible())
                                                   .playersVisible(session.getPlayersVisible())
+                                                  .voteResult(voteResult)
                                                   .version(session.getVersion())
                                                   .created(session.getCreated())
                                                   .updated(session.getUpdated())
@@ -61,6 +73,25 @@ public class SessionUpdateDTO implements UniqueIdentifiable {
             String value = (!session.getVotesVisible()) ? "##HIDE##" : e.getValue();
             sessionUpdate.votes.add(new String[]{key, value});
         });
+    }
+
+    private static String getVoteResult(final SessionDTO session) {
+
+        String result = null;
+        Map<String, Long> collect = session.getVotes()
+                                           .values()
+                                           .stream()
+                                           .filter(StringUtils :: hasText)
+                                           .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        Optional<Long> max = collect.values().stream().max(Comparator.naturalOrder());
+
+        if(max.isPresent()){
+            result = collect.entrySet().stream().filter(e -> e.getValue().equals(max.get())).map(Map.Entry :: getKey).map(String :: valueOf).collect(Collectors.joining(","));
+            result = result + " with " + max.get() + " votes";
+        }
+
+        return result;
     }
 
     @EqualsAndHashCode.Exclude
