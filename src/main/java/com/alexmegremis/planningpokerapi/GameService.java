85acaab2210274893fef8619289c45537c80b0fe
Alex Megremis <alex@aamegremis.com>
2021-07-11 23:31:42 +0000

    Owner can now reset votes. Version advance

--- src/main/java/com/alexmegremis/planningpokerapi/GameService.java
+++ src/main/java/com/alexmegremis/planningpokerapi/GameService.java
@@ -3,7 +3,6 @@ package com.alexmegremis.planningpokerapi;
 import com.alexmegremis.planningpokerapi.api.model.*;
 
 import java.util.Collection;
-import java.util.Map;
 
 public interface GameService {
     <T extends UniqueIdentifiable> String getUniqueId(final Collection<T> existing);
@@ -11,14 +10,14 @@ public interface GameService {
     MessageDTO<SessionUpdateDTO> joinSession(final SessionDTO message, final String userSessionId);
     PlayerDTO createPlayer (final PlayerDTO message, final String userSessionId);
     MessageDTO<PlayerDTO> reconnectPlayer (final PlayerDTO message, final String userSessionId);
-    MessageType vote(final VoteDTO message, final String userSessionId);
+    MessageDTO<String> vote(final VoteDTO message, final String userSessionId);
     MessageDTO<SessionUpdateDTO> getSessionUpdate(final String gameSessionID, final String userSessionId);
     MessageDTO<SessionUpdateDTO> getSessionUpdate(final String gameSessionID);
     MessageDTO<SessionUpdateDTO> openVoting(final String gameSessionID, final String userSessionId);
     MessageDTO<SessionUpdateDTO> closeVoting(final String gameSessionID, final String userSessionId);
-    MessageDTO<SessionUpdateDTO> resetVoting(final String gameSessionID, final String userSessionId);
     MessageDTO<SessionUpdateDTO> showVotes(final String gameSessionID, final String userSessionId);
     MessageDTO<SessionUpdateDTO> hideVotes(final String gameSessionID, final String userSessionId);
     MessageDTO<SessionUpdateDTO> showPlayers(final String gameSessionID, final String userSessionId);
     MessageDTO<SessionUpdateDTO> hidePlayers(final String gameSessionID, final String userSessionId);
+    MessageDTO<SessionUpdateDTO> resetVotes(final String gameSessionID, final String userSessionId);
 }
